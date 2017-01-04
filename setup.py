170f9b815157bed3ee62f11c935704e3cd4d5a2a
craigmbooth <craigmbooth@gmail.com>
2017-01-03 21:21:34 -0600

    Got packaging working

--- setup.py
+++ setup.py
@@ -4,17 +4,13 @@ import sys
 
 PACKAGE_NAME = "nix_visualize"
 
-__version__ = "0.0.1"
-
 setuptools.setup(
     name=PACKAGE_NAME,
-    version=__version__,
-    package_dir={"": "src"},
-    packages=setuptools.find_packages("src"),
-    provides=setuptools.find_packages("src"),
+    version="1.0",
+    packages=["nix_visualize"],
     install_requires=open("requirements.txt").readlines(),
     data_files = [],
     entry_points={"console_scripts": [
-        "nix_visualize=nix_visualize.visualize_tree:main",
+        "nix-visualize=nix_visualize.visualize_tree:main"
     ]}
 )
