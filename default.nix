5bbfd64ea5c12e8aebe0f88a87d4cdd8869d5f32
Allen Nelson <anelson@narrativescience.com>
2017-01-16 12:10:39 -0600

    add default.nix

---
+++ default.nix
@@ -0,0 +1,16 @@
+{
+  pkgs ? import <nixpkgs> {},
+  pythonPackages ? pkgs.python2Packages
+}:
+
+pythonPackages.buildPythonPackage rec {
+  name = "nix-visualize-${version}";
+  version = "1.0.3";
+  src = ./.;
+  propagatedBuildInputs = with pythonPackages; [
+    matplotlib
+    networkx
+    pygraphviz
+    tkinter
+  ];
+}
