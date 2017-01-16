6036344f2cf1a3e00cdbfcb6f02bdc85c19e7f97
craigmbooth <craigmbooth@gmail.com>
2017-01-15 19:25:31 -0600

    Changes to setup.py for PyPI

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ import setuptools
 import sys
 
 PACKAGE_NAME = "nix_visualize"
-
+VERSION = "1.0.0"
 setuptools.setup(
     name=PACKAGE_NAME,
-    version="1.0",
-    packages=["nix_visualize"],
+    version=VERSION,
+    packages=[PACKAGE_NAME],
+    description="CLI to automate generation of pretty Nix dependency trees",
+    author="Craig Booth",
+    author_email="craigmbooth@gmail.com",
+    url="https://github.com/craigmbooth/nix-dependency-visualizer",
+    download_url = "https://github.com/craigmbooth/nix-dependency-visualizer/tarball/"+VERSION,
+    keywords=["nix", "matplotlib"],
+    classifiers=[],
     install_requires=open("requirements.txt").readlines(),
     data_files = [],
     entry_points={"console_scripts": [
